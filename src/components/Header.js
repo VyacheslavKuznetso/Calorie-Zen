06487b5dde0c1affb1adc3fab36568f0fae3ffeb
Vyacheslav Kuznetsov <kuz.poch@yandex.ru>
2023-08-12 19:43:45 +0400

    +

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import headerLogo from '../images/calorieZen.png'
 import './Header.css'
 
 function Header() {
-    return (
-      <div className="header">
-        <img alt="Calorie Zen logo" className="header__logo" src={headerLogo} />
-        <h1 className="header__header">Calorie-Zen</h1>
-      </div>
-    );
-  }
-  
-  export default Header;
+  return (
+    <div className="header">
+      <img alt="Calorie Zen logo" className="header__logo" src={headerLogo} />
+      <h1 className="header__header">Calorie-Zen</h1>
+    </div>
+  );
+}
+
+export default Header;
